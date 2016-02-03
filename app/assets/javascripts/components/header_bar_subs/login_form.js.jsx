5095049649672b297dd1780f48113d5eb5ecfa1e
andrew <azarkon@gmail.com>
2016-02-03 15:05:31 -0500

    fix enter behavior on login and create account views and add useful failure messages

--- app/assets/javascripts/components/header_bar_subs/login_form.js.jsx
+++ app/assets/javascripts/components/header_bar_subs/login_form.js.jsx
@@ -24,14 +24,37 @@
   loginUser: function () {
     TA.AjaxUtil.API.login({username: this.state.username,
                            password: this.state.password},
-                          this.props.voidModal);
+                          this.props.voidModal,
+                          this.tryAgain);
+
+  },
+
+  tryAgain: function() {
+    this.setState($.extend({}, this.getInitialState, {error: true}));
+  },
+
+  enterOrEscape: function(e) {
+    switch(e.charCode) {
+    case 13:
+      e.preventDefault();
+      e.stopPropagation();
+      this.loginUser();
+      break;
+    case 27:
+      this.props.voidModal();
+    }
 
   },
 
     render: function () {
       return(
+<<<<<<< 328ba2b45267a244188a639f6712d8b4cb24c863
         <form onSubmit={this.loginUser}>
         <strong>{this.props.message}</strong>
+=======
+        <form onSubmit={this.loginUser} onKeyPress={this.enterOrEscape}>
+        {(this.state.error) ? <strong> Something went wrong.  :-(  Please try again. </strong> : ""}
+>>>>>>> fix enter behavior on login and create account views and add useful failure messages
           <label>Username
             <input  type="text"
                     value={this.state.username}
